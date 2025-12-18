a114a3f4cf17609580d8c5710986e0fbed8a8e0d
Abdur-RahmanIslam2010 <abdelrahmanislam116@gmail.com>
2025-12-18 22:47:29 +0200

    Create ANN Model & Training it & Predicting

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.layers import Dense
 from tensorflow.keras.models import Sequential
-import string
-import re
+from numpy import argmax
 
 df = pd.read_csv('dataset.csv', encoding='latin1')
 df.columns = ['sentiment', 'id', 'date', 'flag', 'user', 'text']
 X = df['text']
-y = df['sentiment']
-
-print(df.head())
-print(f'\n{df.shape}')
-
-print(f"\n\nX:\n\n{X.head()}\n\ny:\n\n{y.head()}\n")
+y = df['sentiment'] / 2
 
 X_main, X_test, y_main, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 X_train, X_val, y_train, y_val = train_test_split(X_main, y_main, test_size=0.1)
@@ -22,8 +16,26 @@ X_train, X_val, y_train, y_val = train_test_split(X_main, y_main, test_size=0.1)
 vectorizer = TfidfVectorizer()
 
 X_train_vec = vectorizer.fit_transform(X_train)
-X_test_vec = vectorizer.fit_transform(X_test)
-X_val_vec = vectorizer.fit_transform(X_val)
+X_test_vec = vectorizer.transform(X_test)
+X_val_vec = vectorizer.transform(X_val)
 
-print(f"\n\nX_train_vec:\n\n{X_train_vec}\n\nX_test_vec:\n\n{X_test_vec}\n\nX_val_vec:\n\n{X_val_vec}\n")
-print(vectorizer.fit_transform(X).shape)
+print(X_train_vec.shape)
+
+model = Sequential([
+    Dense(128, activation='relu'),
+    Dense(160, activation='relu'),
+    Dense(160, activation='relu'),
+    Dense(128, activation='relu'),
+    Dense(3, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(X_train_vec, y_train, epochs=10, batch_size=32, validation_data=(X_val_vec, y_val), verbose=1)
+
+example = vectorizer.transform([input("Input prediction: ")])
+prediction = model.predict(example)
+labels = ["negative", "neutral", "positive"]
+
+print(prediction)
+print(labels[argmax(prediction)])
