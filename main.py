1e232863a514b736f4f02a92491801d6f7263ecc
Abdur-RahmanIslam2010 <abdelrahmanislam116@gmail.com>
2025-12-18 21:06:57 +0200

    Add word vectorization & data splitting
    TfidfVectorizer already tokenizes the sentences, so word_tokenize was removed. The data is also split into training, testing, and validating
    to prepare it for training the model.

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 import pandas as pd
-from nltk import word_tokenize
-# import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
 from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.models import Sequential
 import string
 import re
 
-df = pd.read_csv('dataset.csv')
-
-# nltk.download('all')     # If 'punkt_tab' is missing, run: nltk.download('punkt_tab')
-
-print("\n", df.head())
-
+df = pd.read_csv('dataset.csv', encoding='latin1')
+df.columns = ['sentiment', 'id', 'date', 'flag', 'user', 'text']
 X = df['text']
 y = df['sentiment']
 
-print(f"\nX:\n{X.head()}\ny:\n{y.head()}\n")
+print(df.head())
+print(f'\n{df.shape}')
 
-tokenized_df = pd.DataFrame(columns=['text'])
+print(f"\n\nX:\n\n{X.head()}\n\ny:\n\n{y.head()}\n")
 
-for row in X:
+X_main, X_test, y_main, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(X_main, y_main, test_size=0.1)
 
-    tokens = word_tokenize(row)
-    tokens = [token for token in tokens if token not in string.punctuation]
-    tokens = [re.sub(r'[^a-zA-Z]', '', token) for token in tokens if re.sub(r'[^a-zA-Z]', '', token)]
-    tokenized_df.loc[len(tokenized_df)] = ''.join(tokens)
-    
-print(tokenized_df.head())
+vectorizer = TfidfVectorizer()
+
+X_train_vec = vectorizer.fit_transform(X_train)
+X_test_vec = vectorizer.fit_transform(X_test)
+X_val_vec = vectorizer.fit_transform(X_val)
+
+print(f"\n\nX_train_vec:\n\n{X_train_vec}\n\nX_test_vec:\n\n{X_test_vec}\n\nX_val_vec:\n\n{X_val_vec}\n")
+print(vectorizer.fit_transform(X).shape)
